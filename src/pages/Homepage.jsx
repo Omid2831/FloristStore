0c80d3acc392d4490a8f177e81d176fad1eaeef1
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-21 10:21:57 +0100

    feat: create Homepage component with RootLayout

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+import RootLayout from '../layout/RootLayout'
+
+const Homepage = () => {
+  return (
+    <RootLayout>
+        {/** Sliders */}
+    </RootLayout>
+  )
+}
+
+export default Homepage
