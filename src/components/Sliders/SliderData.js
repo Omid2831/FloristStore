b82b6b1300223c3af84aabc704e15d436cb7cf18
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-21 11:03:12 +0100

    feat: add slider data for the Swiper component

---
+++ src/components/Sliders/SliderData.js
@@ -0,0 +1,14 @@
+export const sliderData = [
+    {
+        id: 1,
+        image: 'https://via.placeholder.com/800x400?text=Slide+1',
+        title: 'Slide Title 1',
+        subtitle: 'This is the subtitle for slide 1'
+    },
+    {
+        id: 2,
+        image: 'https://via.placeholder.com/800x400?text=Slide+2',
+        title: 'Slide Title 2',
+        subtitle: 'This is the subtitle for slide 2'
+    }
+];
