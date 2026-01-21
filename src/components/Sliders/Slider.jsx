d532d4fb1bd17737cb2f4c217533e1ce2b2ed3b8
Omid2831 <mehrabiomid8282@gmail.com>
2026-01-21 16:48:22 +0100

    feat: remove border styles from Shops and Slider components for cleaner layout

--- src/components/Sliders/Slider.jsx
+++ src/components/Sliders/Slider.jsx
@@ -29,8 +29,8 @@ const Slider = () => {
             fetchData();
         }, []);
     return (
-        <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 border-4 border-dashed border-gray-300 py-12 px-8 gap-5'>
-            <div className='flex justify-center items-center border-4 border-dashed border-gray-400 py-4 px-8 '>
+        <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 py-12 px-8 gap-5'>
+            <div className='flex justify-center items-center py-4 px-8 '>
                 <SliderContext />
             </div>
 
